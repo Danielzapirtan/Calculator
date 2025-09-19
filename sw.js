d461cc59d93fc5114fe688e6a87b8ba8485edae0
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2025-09-19 23:04:48 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'scientific-calculator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event with network-first strategy
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function(response) {
+        // If successful, add to cache
+        return caches.open(CACHE_NAME)
+          .then(function(cache) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+      })
+      .catch(function() {
+        // If network fails, return from cache
+        return caches.match(event.request);
+      })
+  );
+});
