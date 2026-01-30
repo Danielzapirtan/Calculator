a570f04adafeb8bcebb5bcd07abb20202124f083
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-01-30 14:13:38 +0000

    Add file libcalc.h

---
+++ libcalc.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef double NUMBER;
+#define FORMAT "%.6lf"
+
+extern NUMBER add(NUMBER x, NUMBER y);
+extern NUMBER sub(NUMBER x, NUMBER y);
+extern NUMBER mul(NUMBER x, NUMBER y);
+extern NUMBER dvd(NUMBER x, NUMBER y);
+
+extern NUMBER sqrt(NUMBER);
+
